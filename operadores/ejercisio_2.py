3430357a650681cc9985c96476006abb4976323e
Pablo Perez <pablo.perezm@upb.edu.co>
2026-02-27 14:35:19 -0500

    operadores git commit -m operadores

---
+++ operadores/ejercisio_2.py
@@ -0,0 +1,8 @@
+altura_visitante = float(input("ingrese su altura en centimetros"))
+altura_minima = 150
+
+if altura_visitante >= 150:
+    print("se puede lanzar por el tobogan",altura_visitante)
+else:
+    print("no se puede lanza<r, la altura minima es: ",altura_minima)
+    
