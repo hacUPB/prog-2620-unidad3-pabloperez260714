6df5d55d1b04c0ad8ab1302cb90ea325104f14e1
Pablo Perez <pablo.perezm@upb.edu.co>
2026-03-06 15:34:49 -0500

    commits

---
+++ bucles/tablas.py
@@ -0,0 +1,4 @@
+## el primer valor, es desde donde empieza, el segundo valor es hasta donde llega sin incluirlo y el tercer valor es para decir de cuanto va  
+numero = int(input("ingrese el numero a multiplicar: "))
+for i in range (1,11,1):
+    print(f"{numero} x {i} =  {numero*1}")
